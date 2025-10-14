8692b14887118c01390913253174208d328b36b8
unknown <robbjullian730@gmail.com>
2025-10-14 14:07:24 +0800

    edited login bg and made glass

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom';
 import { signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from "../../firebaseConfig";
+import bg from './homepage-comp/images/bglogin.png'
 
 const Login = () => {
     const [email, setEmail] = useState("");
@@ -39,10 +40,11 @@ const Login = () => {
         }
     }
   return (
-    <div className='bg-olive-dark h-screen flex justify-center items-center'>
-      <div className='bg-beige p-10 rounded-lg shadow-2xl w-96 h-auto'>
+    <div className="h-screen flex justify-center items-center bg-cover bg-center"
+  style={{ backgroundImage: `url(${bg})` }}>
+      <div className="bg-white/20 backdrop-blur-md border border-white/30 p-10 rounded-2xl shadow-2xl w-96 h-auto">
         <div className='flex items-center justify-center'>
-            <h1 className='text-2xl font-bold text-olive-dark'>Login</h1>
+            <h1 className='text-2xl font-bold text-white'>Login</h1>
         </div>
         <div className='flex flex-col mt-5 py-10 gap-y-3'>
             <input value={email} onChange={(e) => setEmail(e.target.value)} type="text" placeholder='Email' className='border-2 border-olive rounded-lg p-2 w-72 mb-4'/>
